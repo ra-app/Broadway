0651ffaff27e5637ed3d6c02a260c4f721060a8d
Jimmy <myjimmy@protonmail.com>
2021-09-21 10:06:34 +0300

    BUILD-WASM: Added the bash script file for building the wasm file.

---
+++ Decoder/build_wasm.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+#EMSDK_VERSION="tot-upstream"
+EMSDK_VERSION="latest"
+
+c_files="$(ls ./src/*.c)"
+exported_functions='-s EXPORTED_FUNCTIONS=["_broadwayGetMajorVersion","_broadwayGetMinorVersion","_broadwayInit","_broadwayExit","_broadwayCreateStream","_broadwayPlayStream","_broadwayOnHeadersDecoded","_broadwayOnPictureDecoded"]'
+EXPORT_FLAGS="$exported_functions"
+
+#######################################
+# Ensures a repo is checked out.
+# Arguments:
+#   url: string
+#   name: string
+# Returns:
+#   None
+#######################################
+ensure_repo() {
+  local url name
+  local "${@}"
+
+  git -C ${name} pull || git clone ${url} ${name}
+}
+
+ensure_emscripten() {
+  ensure_repo url='https://github.com/emscripten-core/emsdk.git' name='emsdk'
+  pushd 'emsdk'
+  ./emsdk update-tags
+  ./emsdk install ${EMSDK_VERSION}
+  ./emsdk activate ${EMSDK_VERSION}
+  source ./emsdk_env.sh
+  popd
+}
+
+build() {
+    emcc $c_files -O3 --memory-init-file 1 -s NO_EXIT_RUNTIME=1 -s NO_FILESYSTEM=1 -s TOTAL_MEMORY=52428800 -s ALLOW_MEMORY_GROWTH=1 -s INVOKE_RUN=0 -s DOUBLE_MODE=0 -s AGGRESSIVE_VARIABLE_ELIMINATION=1 -s ALIASING_FUNCTION_POINTERS=1 -s DISABLE_EXCEPTION_CATCHING=1 --js-library library.js $EXPORT_FLAGS -Isrc -Iinc -o ./js/avc.js
+}
+
+main() {
+  ensure_emscripten
+  build
+}
+
+main
